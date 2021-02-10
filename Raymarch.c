2b24427b981b60605a8bb28301960795a1d5c024
Xern <xernerac@gmail.com>
2021-02-10 21:58:38 +0100

    Improved the C version - Removed the result from the Raymarch function since it was never used - Made some functions inline - Refactored the code a bit

--- Raymarch.c
+++ Raymarch.c
@@ -1,8 +1,5 @@
-
-
 #include <stdio.h>
 #include <math.h>
-#include <stdbool.h>
 
 #define MAX_ITTERS 100
 #define MIN_DIST 0.001
@@ -30,7 +27,7 @@ struct ray {
 };
 typedef struct ray ray;
 
-vec3 vec3_c(double x, double y, double z) {
+inline static vec3 vec3_c(double x, double y, double z) {
 	vec3 a;
 	a.x = x;
 	a.y = y;
@@ -38,11 +35,15 @@ vec3 vec3_c(double x, double y, double z) {
 	return a;
 }
 
-double DE_Sphere(vec3 point, vec3 sphere_pos, double sphere_radius) {
-	return pow((sphere_pos.x-point.x)*(sphere_pos.x-point.x)+(sphere_pos.y-point.y)*(sphere_pos.y-point.y)+(sphere_pos.z-point.z)*(sphere_pos.z-point.z),0.5)-sphere_radius;
+inline static double DE_Sphere(vec3 point, vec3 sphere_pos, double sphere_radius) {
+	return sqrt(
+		(sphere_pos.x-point.x) * (sphere_pos.x-point.x) +
+		(sphere_pos.y-point.y) * (sphere_pos.y-point.y) +
+		(sphere_pos.z-point.z) * (sphere_pos.z-point.z)
+	) - sphere_radius;
 }
 
-double DE_Plane(vec3 point, double height) {
+inline static double DE_Plane(vec3 point, double height) {
 	return point.y - height;
 }
 
@@ -56,7 +57,7 @@ double DE(vec3 point) {
 }
 
 
-double dot(vec3 a, vec3 b) {
+inline static double dot(vec3 a, vec3 b) {
 	return a.x * b.x + a.y * b.y + a.z * b.z;
 }
 
@@ -70,7 +71,11 @@ vec3 getNormal(vec3 point) {
 		dist - DE(vec3_c(point.x, point.y, point.z - NORMAL_OFFSET))
 	);
 
-	double len = pow((n.x) * (n.x) + (n.y) * (n.y) + (n.z) * (n.z), 0.5);
+	double len = sqrt(
+		(n.x) * (n.x) +
+		(n.y) * (n.y) +
+		(n.z) * (n.z)
+	);
 	if (len == 0) return vec3_c(0, 0, 0);
 	n.x /= len;
 	n.y /= len;
@@ -78,72 +83,55 @@ vec3 getNormal(vec3 point) {
 	return n;
 }
 
-bool raymarch(ray* ray, int* iterationCount) {
-	for (int i = 0; i < MAX_ITTERS; i++) {
+int raymarch(ray* ray) {
+	int i;
+	for (i = 0; i < MAX_ITTERS; i++) {
 		double dist = DE(ray->startpos);
-		*iterationCount = i;
-		//printf("one it\n");
-		if (dist <= MIN_DIST) return true;
+		if (dist <= MIN_DIST) return i;
 		ray->startpos.x += ray->direction.x * dist;
 		ray->startpos.y += ray->direction.y * dist;
 		ray->startpos.z += ray->direction.z * dist;
 	}
-	return false;
+	return i;
 }
 
-
-
-
-
-
-char indexx[] = { ' ', '.', '-', '*', 'o', '0', 'X' };
-
-
+char chars[] = { ' ', '.', '-', '*', 'o', '0', 'X' };
 
 int main() {
 	ray ray;
 	for (int y = RES_Y-1; y >= 0; y--) {
 		for (int x = 0; x < RES_X; x++) {
-			double u = (double)x / (double)(RES_X - 1);
-			double v = (double)y / (double)(RES_Y - 1);
-
-			u = u * 2 - 1;
-			v = v * 2 - 1;
+			double u = (double)x / (double)(RES_X - 1) * 2 - 1;
+			double v = (double)y / (double)(RES_Y - 1) * 2 - 1;
 
 			vec3 dir = vec3_c(u, v, -1);
-			double len = pow((dir.x) * (dir.x) + (dir.y) * (dir.y) + (dir.z) * (dir.z), 0.5);
+			double len = sqrt(
+				(dir.x) * (dir.x) +
+				(dir.y) * (dir.y) +
+				(dir.z) * (dir.z)
+			);
 			dir.x /= len;
 			dir.y /= len;
 			dir.z /= len;
 
 			ray.direction = dir;
 			ray.startpos = vec3_c(0, 0, 2);
-			int iterations = 0;
 
-			bool res = raymarch(&ray, &iterations);
+			const int iterations = raymarch(&ray);
 
 			vec3 n = getNormal(ray.startpos);
 
-			double color;
-
 			// ambiant occlusion
-			double ao = pow(AO_BASE, -iterations);
+			const double ao = pow(AO_BASE, -iterations);
 
 			// Phong Shading
 			double light = dot(n, vec3_c(0.5773502691896258, 0.5773502691896258, 0.5773502691896258));
 			if (light < 0) light = 0;
 
-			color = ao * light;
+			const double color = ao * light;
 
-			putchar(indexx[(int)round(color * 6)]);
-			/*
-			if (res) {
-				putchar(219);
-			} else {
-				putchar(32);
-			}*/
+			putchar(chars[(int)round(color * 6)]);
 		}
-		putchar(10);
+		putchar('\n');
 	}
-	
 }
