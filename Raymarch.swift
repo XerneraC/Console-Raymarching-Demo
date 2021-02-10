def351bf567bc545ac6d47a87f65f706afd1b746
Xern <xernerac@gmail.com>
2021-02-10 22:48:03 +0100

    Changed everything to SIMD It's not working tho...

--- Raymarch.swift
+++ Raymarch.swift
@@ -14,21 +14,33 @@ struct vec3 {
 	var z = 0.0
 }
 
+func length(_ a: SIMD3<Double>) -> Double {
+	return (a*a).sum().squareRoot()
+}
+
+func distance(_ a: SIMD3<Double>, _ b: SIMD3<Double>) -> Double {
+	return length(a - b)
+}
+
+func dot(_ a: SIMD3<Double>, _ b: SIMD3<Double>) -> Double {
+	return (a * b).sum()
+}
+
 struct ray {
-	var startpos = vec3()
-	var direction = vec3()
+	var startpos = SIMD3<Double>()
+	var direction = SIMD3<Double>()
 }
 
-func DE_Sphere(_ point: vec3, _ sphere_pos: vec3, _ sphere_radius: Double) -> Double {
-	return ((sphere_pos.x-point.x)*(sphere_pos.x-point.x)+(sphere_pos.y-point.y)*(sphere_pos.y-point.y)+(sphere_pos.z-point.z)*(sphere_pos.z-point.z)).squareRoot() - sphere_radius
+func DE_Sphere(_ point: SIMD3<Double>, _ sphere_pos: SIMD3<Double>, _ sphere_radius: Double) -> Double {
+	return distance(point, sphere_pos) - sphere_radius
 }
 
-func DE_Plane(_ point: vec3, _ height: Double) -> Double {
+func DE_Plane(_ point: SIMD3<Double>, _ height: Double) -> Double {
 	return point.y - height
 }
 
-func DE(_ point: vec3) -> Double {
-	let sphere1 = vec3(x: 0, y: 0, z: 0)
+func DE(_ point: SIMD3<Double>) -> Double {
+	let sphere1 = SIMD3<Double>(0, 0, 0)
 	let s = DE_Sphere(point, sphere1, 1)
 	let p = DE_Plane(point, -1)
 	return s < p ? s : p
@@ -38,26 +50,22 @@ func dot(_ a: vec3, _ b: vec3) -> Double {
 	return a.x * b.x + a.y * b.y + a.z * b.z
 }
 
-func getNormal(_ point: vec3) -> vec3 {
+func getNormal(_ point: SIMD3<Double>) -> SIMD3<Double> {
 	let dist = DE(point)
-	var n = vec3(
-		x: dist - DE(vec3(x: point.x - NORMAL_OFFSET, y: point.y, z: point.z)),
-		y: dist - DE(vec3(x: point.x, y: point.y - NORMAL_OFFSET, z: point.z)),
-		z: dist - DE(vec3(x: point.x, y: point.y, z: point.z - NORMAL_OFFSET))
+	var n = SIMD3<Double>(
+		dist - DE(point + SIMD3<Double>(NORMAL_OFFSET, 0, 0)),
+		dist - DE(point + SIMD3<Double>(0, NORMAL_OFFSET, 0)),
+		dist - DE(point + SIMD3<Double>(0, 0, NORMAL_OFFSET))
 	)
 
-	let len = ((n.x) * (n.x) + (n.y) * (n.y) + (n.z) * (n.z)).squareRoot()
-	if (len == 0) { return vec3(x: 0, y: 0, z: 0) }
-	n.x /= len
-	n.y /= len
-	n.z /= len
-	return n
+	let len = length(n)
+	if (len == 0) { return SIMD3<Double>(0, 0, 0) }
+	return n / len
 }
 
 struct raymarchResult {
 	var outRay = ray()
-	var iterationCount = 0
-	var result = false
+	var iterationCount: Int = 0
 }
 
 func raymarch(_ rayV: ray) -> raymarchResult {
@@ -67,14 +75,10 @@ func raymarch(_ rayV: ray) -> raymarchResult {
 		let dist = DE(result.outRay.startpos)
 		result.iterationCount = i
 		if (dist <= MIN_DIST) {
-			result.result = true
 			return result
 		}
-		result.outRay.startpos.x += result.outRay.direction.x * dist
-		result.outRay.startpos.y += result.outRay.direction.y * dist
-		result.outRay.startpos.z += result.outRay.direction.z * dist
+		result.outRay.startpos += result.outRay.direction * dist
 	}
-	result.result = false
 	return result
 }
 
@@ -84,20 +88,15 @@ func main() {
 	var rayV = ray()
 	for y in (0...RES_Y).reversed() {
 		for x in 0..<RES_X {
-			var u = Double(x) / Double(RES_X - 1)
-			var v = Double(y) / Double(RES_Y - 1)
+			var u = Double(x) / Double(RES_X - 1) * 2 - 1
+			var v = Double(y) / Double(RES_Y - 1) * 2 - 1
 
-			u = u * 2 - 1
-			v = v * 2 - 1
-
-			var dir = vec3(x: u, y: v, z: -1)
-			let len = ((dir.x) * (dir.x) + (dir.y) * (dir.y) + (dir.z) * (dir.z)).squareRoot()
-			dir.x /= len
-			dir.y /= len
-			dir.z /= len
+			var dir = SIMD3<Double>(u, v, -1)
+			let len = length(dir)
+			dir /= len
 
 			rayV.direction = dir
-			rayV.startpos = vec3(x: 0, y: 0, z: 2)
+			rayV.startpos = SIMD3<Double>(0, 0, 2)
 
 			let ress = raymarch(rayV)
 			let iterations = ress.iterationCount
@@ -107,7 +106,7 @@ func main() {
 
 			let ao = pow(AO_BASE, Double(-iterations))
 
-			var light = dot(n, vec3(x: 0.5773502691896258, y: 0.5773502691896258, z: 0.5773502691896258))
+			var light = dot(n, SIMD3<Double>(0.5773502691896258, 0.5773502691896258, 0.5773502691896258))
 			if (light < 0) { light = 0 }
 
 			var color: Double = ao * light * 6
